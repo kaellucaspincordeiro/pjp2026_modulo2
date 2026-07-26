1d307cb8b1da1a4e4cb930fd682e7e043ae9aa39
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:39:46 -0300

    Quadrado

---
+++ Exercícios_aula03/Laço FOR/ex35.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 35 - Quadrado</title>
+</head>
+<body>
+
+    <?php
+
+        for ($i=1; $i <= 5 ; $i++) { 
+            for ($j=1; $j <= 5; $j++) { 
+                echo "*";
+            }
+            echo "<br>";
+        }
+
+    ?>
+
+</body>
+</html>
