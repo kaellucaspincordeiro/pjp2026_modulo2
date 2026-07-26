c2750b3d2fc6ae23ea11c4e569bc51081a8d96b6
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:46:43 -0300

    Maior valor

---
+++ Exercícios_aula03/Arrays/ex44.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 44 - Maior Valor com Array</title>
+</head>
+<body>
+
+<?php
+
+$numeros = [1,2,3,4,5,6,7,8,9,10];
+
+    $maior = max($numeros);
+
+    echo "O maior valor é ".$maior;
+
+?>
+
+</body>
+</html>
