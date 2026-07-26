4aa8fa2a379ca79dccfbb59ed4c8a14b78313841
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:37:25 -0300

    Somar os números de 1 até 100

---
+++ Exercícios_aula03/Laço FOR/ex31.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 30</title>
+</head>
+<body>
+
+    <?php
+
+        $soma = 0;
+
+        for ($i=1; $i <= 100 ; $i++) { 
+            $soma = $soma + $i;
+        }
+
+        echo "O somatório de 1 até 100 resultou em ".$soma;
+
+    ?>
+
+</body>
+</html>
