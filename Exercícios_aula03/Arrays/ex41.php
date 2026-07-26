ba6499423c14846d0ca0b571358fce62c8f48366
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:44:57 -0300

    Array com 10 nomes

---
+++ Exercícios_aula03/Arrays/ex41.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 41 - Nomes das pessoas</title>
+</head>
+<body>
+
+<?php
+
+$nomes = ["Pablo", "Kael", "Jucileia", "Carmen", "Haruo", "Carlos", "Thiago", "Sofia", "Edgar", "Ivan"];
+
+    foreach($nomes as $chave){
+        echo "$chave <br><br>";
+    }
+
+?>
+
+</body>
+</html>
