5fe04194b63b9aba2cae857db3d1fb97cf0853f0
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:19:27 -0300

    Olá Mundo em PHP

---
+++ Exercícios_aula03/Básico/ex1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 1</title>
+</head>
+<body>
+
+<?php
+
+    echo "Olá Mundo!";
+
+?>
+
+</body>
+</html>
