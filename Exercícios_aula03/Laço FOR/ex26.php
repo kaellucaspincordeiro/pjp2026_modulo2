683ca720b602d59d50062315bbda9d9d8cb2142e
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:34:48 -0300

    Incremento

---
+++ Exercícios_aula03/Laço FOR/ex26.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 26 - Incremento</title>
+</head>
+<body>
+    
+    <?php
+
+        for ($i=1; $i <= 10; $i++) { 
+            echo $i." ";
+        }
+
+    ?>
+
+</body>
+</html>
