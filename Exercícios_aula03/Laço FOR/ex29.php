13066b15c809ecddbd2eac60b2764c1b025d665b
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:36:18 -0300

    Laço FOR apenas ímpares

---
+++ Exercícios_aula03/Laço FOR/ex29.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 29 - Números Ímpares</title>
+</head>
+<body>
+
+    <?php
+
+        for ($i=0; $i <= 100 ; $i++) { 
+        
+            if($i % 2 != 0){
+                echo $i." ";
+            }
+
+        }
+
+    ?>
+
+</body>
+</html>
