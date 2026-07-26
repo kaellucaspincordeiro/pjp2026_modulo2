cdb958d0bf1195e951dcd9477d5b455c3c034806
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:41:53 -0300

    Contagem regressiva

---
+++ Exercícios_aula03/Laço WHILE e DO WHILE/ex37.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 37 - Contagem Regressiva</title>
+</head>
+<body>
+
+    <?php
+
+        $contador = 11;
+
+        while ($contador >= 1) {
+            $contador -= 1;
+            echo $contador." ";
+        }
+
+    ?>
+
+</body>
+</html>
