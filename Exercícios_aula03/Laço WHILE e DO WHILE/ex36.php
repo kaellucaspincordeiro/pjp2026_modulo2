ee43b9bdadf959fb52390b971250a3d815ee3cf6
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:41:15 -0300

    Mostrar os números de 1 até 20 usando while

---
+++ Exercícios_aula03/Laço WHILE e DO WHILE/ex36.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 36 - While contando de 1 até 20</title>
+</head>
+<body>
+
+    <?php
+
+        $contador = 0;
+
+        while ($contador <= 19) {
+            $contador += 1;
+            echo $contador." ";
+        }
+
+    ?>
+
+</body>
+</html>
