6452c65b9c273294c3d86c31a76fd55aa543d287
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:39:25 -0300

    Pirâmide

---
+++ Exercícios_aula03/Laço FOR/ex34.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 34 - Pirâmide</title>
+</head>
+<body>
+
+    <?php
+
+        for ($i=1; $i <= 5 ; $i++) { 
+            for ($j=1; $j <= $i; $j++) { 
+                echo "*";
+            }
+            echo "<br>";
+        }
+
+    ?>
+
+</body>
+</html>
