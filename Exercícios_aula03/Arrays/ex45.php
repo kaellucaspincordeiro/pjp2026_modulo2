55bf4d6e8ef26c7cf0ce000f1518f96219a6232c
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-26 19:47:01 -0300

    Menor valor

---
+++ Exercícios_aula03/Arrays/ex45.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 45 - Menor Valor com Array</title>
+</head>
+<body>
+
+<?php
+
+$numeros = [1,2,3,4,5,6,7,8,9,10];
+
+    $menor = min($numeros);
+
+    echo "O menor valor é ".$menor;
+
+?>
+
+</body>
+</html>
