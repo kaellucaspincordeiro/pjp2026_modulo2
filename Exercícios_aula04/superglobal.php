e5d18ea8bd859c4a21546dad95c93e371b671cd8
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-29 17:40:35 -0300

    Exercício 2 - Superglobal $_SERVER

---
+++ Exercícios_aula04/superglobal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 2</title>
+</head>
+<body>
+
+<h2>Superglobal e seus parâmetros:</h2>
+
+<?php
+
+    var_dump($_SERVER);
+    echo '<br><br>';
+    echo 'IP: '.$_SERVER['SERVER_ADDR'];
+    echo '<br><br>';
+    echo 'Nome do Arquivo Atual: '.basename($_SERVER['SCRIPT_NAME']);
+    echo '<br><br>';
+    echo 'URL: '.$_SERVER['SERVER_ADDR'].''.$_SERVER['SCRIPT_NAME'];
+    
+
+?>
+
+</body>
+</html>
