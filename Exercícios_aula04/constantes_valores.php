ead0a90c6e946d83af57413023ea04ceb4011f4f
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-07-29 17:40:01 -0300

    Exercício 1 - Constantes e Valores

---
+++ Exercícios_aula04/constantes_valores.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 1</title>
+</head>
+<body>
+
+<h2>Constantes e seus valores:</h2>
+
+<?php
+
+    define('MAIOR_IDADE', 18);
+    define('PERC_ICMS', 25);
+    define('TEXTO_BOASVINDAS', 'Seja Bem Vindo ao nosso site');
+
+    echo MAIOR_IDADE;
+    echo '<br><br>';
+    echo PERC_ICMS;
+    echo '<br><br>';
+    echo TEXTO_BOASVINDAS;
+
+?>
+
+</body>
+</html>
