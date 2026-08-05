aec0f0b9efce72d29ea6873982474219fcf4f1cb
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-08-05 20:25:42 -0300

    Exercício 1 - Função da Potenciação

---
+++ Exercícios_aula05/Exercicio_1/potenciacao.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Potenciação</title>
+</head>
+<body>
+    <?php
+        function potenciacao($base, $expoente){
+            return $base ** $expoente;
+        }
+    ?>
+</body>
+</html>
