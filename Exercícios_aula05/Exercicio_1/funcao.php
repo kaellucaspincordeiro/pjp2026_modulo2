cfb5a0ac86050c0307c0cf344e6411d6c4b22695
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-08-05 20:26:06 -0300

    Incluir o arquivo Potenciação

---
+++ Exercícios_aula05/Exercicio_1/funcao.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <?php
+        include('potenciacao.php');
+
+        echo potenciacao(4,3);
+    ?>
+</body>
+</html>
