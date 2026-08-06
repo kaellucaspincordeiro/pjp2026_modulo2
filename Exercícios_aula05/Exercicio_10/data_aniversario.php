c815795afb701164697f6ee15aaee7d60f819702
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-08-05 21:02:40 -0300

    Imprimir a data do seu aniversário com os três formatos

--- Exercícios_aula05/Exercicio_10/data_aniversario.php
+++ Exercícios_aula05/Exercicio_10/data_aniversario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 9</title>
+    <title>Exercício 10</title>
 </head>
 <body>
     <?php
